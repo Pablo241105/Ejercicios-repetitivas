34f2b40d173861ecc235f18c35b14283dfe43b61
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-10-24 11:15:56 +0200

    a

---
+++ Ejercicios repetitivas Vol I.py
@@ -0,0 +1,8 @@
+numero=0
+resultado=1
+numero=(int)(input("Dime el numero: "))
+
+for numero in range(1,numero+1):
+    resultado= resultado * numero
+
+    print (resultado)
---
+++ Ejercicios repetitivas Vol.III.py
@@ -0,0 +1,6 @@
+
+numero=0
+while(numero!=0):
+    print("Dime un numero")
+    if (numero==0):
+        print(numero+numero)
