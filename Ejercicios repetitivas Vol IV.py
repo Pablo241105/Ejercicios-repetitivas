1cdb986d618d9bfa7b340619019d7f0381187162
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-10-28 11:01:20 +0200

    Clase 28-10-2022

--- Ejercicios repetitivas Vol IV.py
+++ Ejercicios repetitivas Vol IV.py
@@ -1,6 +1,4 @@
 numero=0
 numero=(int)(input("Dime un numero: "))
-
-
 while (numero):
-    
+      
--- Ejercicios repetitivas Vol.III.py
+++ Ejercicios repetitivas Vol.III.py
@@ -1,6 +1,6 @@
 numeros=0
 numeros=(int)(input("Dime un numero"))
 while(numeros!=0):
-   (int)(input("Dime un numero"))
-   if(numeros==0):
-    print
+  (int)(input("Dime un numero"))
+if(numeros==0):
+  print(numeros+0,numeros+0/2)
