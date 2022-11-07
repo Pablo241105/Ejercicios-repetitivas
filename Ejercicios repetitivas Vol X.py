25e3e69282e314f6456164725fed1e6ae4b15f8c
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-11-07 11:09:39 +0100

    Clase 7-11-2022

--- Ejercicios repetitivas Vol X.py
+++ Ejercicios repetitivas Vol X.py
@@ -1,12 +1,6 @@
+for x in range(1,6):
+    for numero in range(1,11)  :
+        print(x*numero)
+         
 
-def multiplicar(numero):
-    numero=x in range(1,5)
-    multiplicacion=0
-    for x in range(1,10):
-        multiplicacion= multiplicacion * numero
-    return multiplicacion
     
- 
-print("Estas son las multiplicaciones: ",multiplicar(numero))
-
-
--- a.py
+++ a.py
@@ -1,10 +1,8 @@
-numero = (int)(input("Dime un número, pulsa el 0 para salir:\n"))
-sumanumeros = 0
-media = 0
-while numero != 0:
-    sumanumeros = sumanumeros + numero
-    media = media + 1
-    numero = (int)(input("Dime otro número, pulsa 0 para salir:\n"))
+numero = int(input("Introduce un número para comprobar si es primo:"))
+for num in range(2, numero):
+    
+    if numero % num == 0:
+        print("Es primo")
+        else:
+            print("No es")
 
-print("La suma de todos lo números es:", sumanumeros)
-print("La media de todos los números es:", sumanumeros/media)
