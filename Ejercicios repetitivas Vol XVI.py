17a04b4d731be9dd8c29b14e2539185c1ac0ac28
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-11-15 12:38:24 -0100

    Clase 15-11-2022

--- Ejercicios repetitivas Vol XVI.py
+++ Ejercicios repetitivas Vol XVI.py
@@ -1,24 +1,17 @@
-contador=1
 total=0
 sueldo=0
 sueldo=int(input("¿Cuanto cobras en una hora?: "))
 trabajadores=0
 trabajadores=(int)(input("¿Cuantos trabajadores hay?: "))
 totaltrabajadores=0
+contador=0
 for x in range(1,7):
-    numero=0
-    numero=int(input("Dime cuantas horas has trabajado el dia :"))
-    contador=contador+1
-    total=total+numero
-
     numero2=0
     numero2=int(input("¿Cuantas horas hacen los trabajadores?: "))    
     contador=contador+1
-    contador=contador*trabajadores
     print("Los trabajadores han trabajado esto: ",totaltrabajadores)
     totaltrabajadores=totaltrabajadores+numero2
-        
 
-print("Has echado estas horas: ", total)
-print("Esta semana has ganado: ", total*sueldo)
+
+
 print("Los trabajadores han ganado: ",totaltrabajadores*sueldo)
---
+++ Ejercicios repetitivas Vol XVIII.py
@@ -0,0 +1,9 @@
+import os
+import time
+for hora in range(24):
+    for minutos in range(60):
+        for segundos in range(60):
+         
+            print(hora,minutos,segundos)
+          
+            time.sleep(1)
