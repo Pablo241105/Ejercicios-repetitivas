5010b6664a01ad524d5a44c3f2369bf9083e61d2
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-11-17 07:57:49 -0100

    u

--- Ejercicios repetitivas Vol XVI.py
+++ Ejercicios repetitivas Vol XVI.py
@@ -4,9 +4,9 @@ sueldo=int(input("¿Cuanto cobras en una hora?: "))
 trabajadores=0
 trabajadores=(int)(input("¿Cuantos trabajadores hay?: "))
 totaltrabajadores=0
-contador=0
+contador=0                                                                                                                                                                                                                                                                                                                                                               
 for x in range(1,7):
-    numero2=0
+    numero2=0                      
     numero2=int(input("¿Cuantas horas hacen los trabajadores?: "))    
     contador=contador+1
     print("Los trabajadores han trabajado esto: ",totaltrabajadores)
