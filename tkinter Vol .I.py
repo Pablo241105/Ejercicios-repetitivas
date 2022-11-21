30daf1ef6cdc94dc514462314abaae057f974b63
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-11-21 08:52:34 -0100

    g

--- tkinter Vol .I.py
+++ tkinter Vol .I.py
@@ -1,16 +1,30 @@
 from tkinter import *
 from tkinter import ttk
+def saludar():
+    texto=campoTexto.get()
+    print(texto)
+
+    
 ventana = Tk()
 ventana.title("Ejercicio 1")
-ventana.geometry("600x500")
+ventana.geometry("250x300")
 ventana.resizable(width=False,height=False)
-ventana.config(background="bisque2")
-ventana.iconbitmap(file="cosa.ico")
+ventana.config(background="cyan")
+
+
 frm = ttk.Frame(ventana).pack()
+
+
 labeltexto=ttk.Label(frm,text="Hola Tkinter")
-labeltexto.config(background="dark slate grey",border=5,font=("Arial",15))
+labeltexto.config(background="gold",border=5,font=("Arial",15))
 labeltexto.pack()
 
+
+
+campoTexto=ttk.Entry(frm)
+campoTexto.pack()
+
+ttk.Button(frm,text="Leer",command=saludar).pack()
 ttk.Button(frm,text="Salir",command=ventana.destroy).pack()
 
 ventana.mainloop()
