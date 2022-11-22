e6cdc98ca91cc0e86ea4276dba9978509a3a24c3
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-11-22 14:40:25 +0100

    Clase 22-11-2022

--- a.py
+++ a.py
@@ -1,8 +1,27 @@
-numero = int(input("Introduce un número para comprobar si es primo:"))
-for num in range(2, numero):
-    
-    if numero % num == 0:
-        print("Es primo")
-        else:
-            print("No es")
+from pytube import YouTube
+from pytube import Playlist
 
+
+def imprimirHola(nombre:str, apellido:str):
+    print("Hola", nombre, apellido)
+
+
+def sumarDosNumeros(num1:int, num2:int):
+    #print("La suma es", num1+num2)
+    return num1+num2
+
+
+def descargaCancion(url:str):
+    youtube = YouTube(url)
+    print(youtube.author)
+    print("Descargando", youtube.title)
+    cancion = youtube.streams.get_audio_only()
+    cancion.download()
+
+def descargarLista(url:str):
+    playlist = Playlist(url)
+
+    for cancion in playlist.videos:
+        print("Descargando cancion: ", cancion.title)
+        cancion.streams.get_audio_only().download("canciones/")
+        print("****************\n")
