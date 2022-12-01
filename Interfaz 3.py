9f7e6ad9d8f11eda762fc50d57880e05b44bc64b
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-12-01 10:14:06 +0100

    Clase 1-12-2022

---
+++ Interfaz 3.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+usuario=""
+contraseña=""
+confirmar=""
+vUsuarios=[]
+
+
+    
+ventana=Tk()
+ventana.title("Pedir ingredientes al usuario")
+ventana.geometry("600x600")
+ventana.resizable(False,False)
+ventana.config(background="gold")
+
+combo=ttk.Combobox(ventana, state="readonly",values=["M","F"])
+combo.place(x=200,y=50)
+
+labelTexto=ttk.Label(ventana,text="Pon tus ingredientes:")
+labelTexto.place(x=220,y=10)
+
+ventana.mainloop()
--- interfaz 2 .py
+++ interfaz 2 .py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
-
 usuario=""
 contraseña=""
 confirmar=""
@@ -12,27 +11,26 @@ def guardardatos():
     usuario=entryusuario.get()
     contraseña=entrycontraseña.get()
     confirmar=entryconfirmar.get()
+    sexo=combo.get()
+    
+    
     if contraseña==confirmar:
         vUsuarios.append(usuario)
         vUsuarios.append(contraseña)
+        vUsuarios.append(sexo)
         print(vUsuarios)
-    messagebox.showinfo("Usuario Guardado",f"Usuario guaraddo")   
-    
-
-
-
-
-
-
+        messagebox.showinfo("Usuario Guardado",f"Usuario,contraseña y sexo guardados")   
 
 ventana=Tk()
 ventana.title("Pedir datos al usuario")
-ventana.geometry("500x500")
+ventana.geometry("600x600")
 ventana.resizable(False,False)
 ventana.config(background="gold")
 
-labelTexto=ttk.Label(ventana,text="Pon tus datos:")
+combo=ttk.Combobox(ventana, state="readonly",values=["M","F"])
+combo.place(x=160,y=150)
 
+labelTexto=ttk.Label(ventana,text="Pon tus datos:")
 
 labelusuario=ttk.Label(ventana,text="Introduce tu usuario")
 entryusuario=ttk.Entry(ventana)
@@ -41,7 +39,6 @@ entrycontraseña=ttk.Entry(ventana,show="*")
 
 botonguardar=ttk.Button(ventana,text="Guardar",command=guardardatos)
 botonsalir=ttk.Button(ventana,text="Salir",command=ventana.destroy)
-
 
 labelTexto.grid(row=0,column=1)
 labelusuario.grid(row=1,column=0)
@@ -55,7 +52,6 @@ entryconfirmar=ttk.Entry(ventana,show="*")
 
 labelconfirmar.grid(row=5,column=0,padx=10,pady=10)
 entryconfirmar.grid(row=5,column=1)
-
 
 botonguardar.grid(row=6,column=1)
 botonsalir.grid(row=6,column=0)
