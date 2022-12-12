8da4c1c1f836d0d4eabbe3986d9bb73475466e36
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2022-12-12 11:14:35 +0100

    Clase 12-12-2022

--- Lista de la compra.py
+++ Lista de la compra.py
@@ -1,35 +1,34 @@
 import flet as ft
-
+vCosas=[]
 def main(page:ft.Page):
-    def cambiarColor(e):
-        for x in range (1000):
-            vCosas[]
-
+    def guardardatos(e):
+      vCosas=[]
+      vCosas.append(Cosa2)
+      vCosas.append(Cosa3)
+      vCosas.append(Cosa4)
+      vCosas.append(Cosa5)
+      print(vCosas)
+       
         
-        page.update()
-        
-    
-    texto=ft.Text(value="ola",color="cyan",size= 150 )
+    texto=ft.Text(value="hola",color="cyan",size= 150 )
     page.add(texto)
     texto.value="Hola,di tu nombre"
     
-    bt=ft.FloatingActionButton(icon=ft.icons.ADD,on_click=cambiarColor)
+    bt=ft.FloatingActionButton(icon=ft.icons.ADD,on_click=guardardatos)
     page.add(bt)
     
     textField_Nombre=ft.TextField(label="Nombre",hint_text="Escribe tu nombre")
     page.add(textField_Nombre)
-    
-    dropDown_Menu=ft.Dropdown(width=100,options=[ft.dropdown.Option("Lexugas")])
+    print(textField_Nombre)
+    dropDown_Menu=ft.Dropdown(width=100,options=[ft.dropdown.Option("Cosa 1")])
     page.add(dropDown_Menu)
-    dropDown_Menu.options.append(ft.dropdown.Option("Tomatoes"))
+    Cosa2=dropDown_Menu.options.append(ft.dropdown.Option("Cosa 2"))
+    Cosa3=dropDown_Menu.options.append(ft.dropdown.Option("Cosa 3"))
+    Cosa4=dropDown_Menu.options.append(ft.dropdown.Option("Cosa 4"))
+    Cosa5=dropDown_Menu.options.append(ft.dropdown.Option("Cosa 5"))
     page.update()
     
- 
     
     page.update()
-
-
-
 
 ft.app(target=main)
-
